9ed5bae69d8438391e8cd2ba57f675b233618bb0
Alen Kapitanker <alengamer76@gmail.com>
2021-10-08 21:30:37 +0300

    more fixes

--- commands/filter.js
+++ commands/filter.js
@@ -1,19 +1,14 @@
 const Command = require("../structs/Command");
 const DB = require("quick.db")
-const ffmpegFilters = {
-    
-}
 module.exports = new Command({
     name: "effect",
     aliases: ["e"],
     description: "Sets a filter to the queue",
     dmOnly: false,
     async run(message, args, client) {
-        let names = Object.keys(ffmpegFilters)
-        let moreNames = Object.keys(client.effects)
-        let combined = [...names, ...moreNames]
-        if(names.includes(args[0])) {
-            
+        let effects = Object.keys(client.effects)
+        if(effects.includes(args[0])) {
+
             let filter = client.distube.setFilter(message, args[0]);
             message.channel.send(`Current queue effect: \`${(filter || "Off")}\``);
         }
